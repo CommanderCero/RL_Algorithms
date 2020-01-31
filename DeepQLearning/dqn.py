ec1436a39eb9909ec9ce26d9be971c8490b46045
djeurissen <dominikjeurissen@web.de>
2020-01-31 12:46:12 +0100

    Implement training and add epochs for logging

--- DeepQLearning/dqn.py
+++ DeepQLearning/dqn.py
@@ -41,18 +41,19 @@ class ReplayMemory:
     
 def default_network_template(obs_shape, act_dim):
     return nn.Sequential(
-        nn.modules.Flatten(),
         nn.Linear(np.prod(obs_shape), 256),
         nn.ReLU(),
-        nn.Linear(256, act_dim)
+        nn.Linear(256, act_dim))
         
 class DQNAgent:
-    def __init__(self, env, network_template=default_network_template,
+    def __init__(self, env_fn, network_template=default_network_template,
                  reward_decay=0.99, memory_size=10000, exploration_steps=2000,
-                 steps_per_epoch=5000, batch_size=32):
-        self.obs_shape = env.observation_space.shape
-        self.act_dim = env.action_space.n
-        self.env = env
+                 steps_per_epoch=250, batch_size=32):
+        self.env_fn = env_fn
+        self.env = env_fn()
+        self.test_env = env_fn()
+        self.obs_shape = self.env.observation_space.shape
+        self.act_dim = self.env.action_space.n
         
         self.reward_decay = reward_decay
         self.exploration_steps = exploration_steps
@@ -62,9 +63,36 @@ class DQNAgent:
         self.memory = ReplayMemory(self.obs_shape, memory_size)
         self.q_network = network_template(self.obs_shape, self.act_dim)
         self.target_q_network = network_template(self.obs_shape, self.act_dim)
+        self.optimizer = torch.optim.RMSprop(self.q_network.parameters(), lr=1e-3)
+        
         self.step = 0
+        
+    def test_run(self, episodes=10, render=False):
+        returns = []
+        ep_lengths = []
+        for i in range(episodes):
+            state = self.test_env.reset()
+            done = False
+            returns.append(0)
+            ep_lengths.append(0)
+            
+            while not done:
+                action = self.select_action(state)
+                state, reward, done, _ = self.test_env.step(action)
+                if render:
+                    self.test_env.render()
+                
+                returns[i] += reward
+                ep_lengths[i] += 1
+                
+        # Close the test environment after rendering to prevent any "dead" windows
+        if render:
+            self.test_env.close()
+            self.test_env = self.env_fn()
+            
+        return (returns, ep_lengths)
     
-    def run(self, total_steps):
+    def train_run(self, total_steps):
         state = self.env.reset()
         for i in range(total_steps):
             
@@ -86,18 +114,24 @@ class DQNAgent:
             
             # End of trajectory
             if done:
-                state = env.reset()
-                
+                state = self.env.reset()
+            
+            if self.step > self.exploration_steps:
                 # Train the q-network
                 batch = self.memory.sample(self.batch_size)
                 self.train(batch)
+                
+                # End of epoch - Log some data about our agent
+                if self.step % self.steps_per_epoch == 0:
+                    returns, ep_lengths = self.test_run()
+                    print("Avg Return: {:<15}Avg Ep-Length: {}".format(np.mean(returns), np.mean(ep_lengths)))
                 
             
     def select_action(self, state):
         # Select action according to epsilon greedy policy
         state_tensor = torch.Tensor([state])
         with torch.no_grad():
-            action = self.q_network.forward(state_tensor)[0].argmax()
+            action = self.q_network(state_tensor)[0].argmax()
             
         return action.item()
     
@@ -111,13 +145,23 @@ class DQNAgent:
         # Calculate the target with the bellman-equation
         # Q(s,a) = r + gamma * max[Q(s', a'))]
         with torch.no_grad():
-            next_q_values = self.q_network.forward(next_states)
-            target = rewards + (1 - done) * self.reward_decay * torch.max(next_q_values, axis=1)
-        return
+            next_q_values = self.q_network(next_states)
+            target = rewards + (1 - done) * self.reward_decay * torch.max(next_q_values, axis=1).values
+        
+        # Calculate the q_values for the old states and the values that we want to change
+        q_values = self.q_network(states)
+        relevant_q_values = torch.gather(q_values, 1, actions.long().view(-1,1)).squeeze()
+        
+        # Train
+        loss = ((relevant_q_values - target.detach()) ** 2).mean()
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
         
 
 if __name__ == "__main__":
-    env = gym.make("CartPole-v0").unwrapped
-    agent = DQNAgent(env)
+    env_fn = lambda: gym.make("CartPole-v0")
+    agent = DQNAgent(env_fn)
     
-    agent.run(10000)
+    agent.train_run(10000)
+    agent.test_run(episodes=10, render=True)
